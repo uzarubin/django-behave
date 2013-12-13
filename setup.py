ec41f4ec74bda3524832f011e07fb99b5ce1100d
uzarubin <ustin.zarubin@campusbellhops.com>
2013-12-13 15:50:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Django Test Runner for the Behave BDD module',
     author='Rachel Willmer',
     author_email='rachel@willmer.org',
-    url='https://github.com/rwillmer/django-behave',
+    url='https://github.com/CampusBellhops/django-behave.git',
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'Framework :: Django',
